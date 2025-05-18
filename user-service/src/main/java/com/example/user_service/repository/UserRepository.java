2885877df8c0380a15bd050937cdb2fe8e03bd18
maherparkar <maherparkar@gmail.com>
2025-05-19 05:45:51 +1000

    Initial commit - added user-service microservice

---
+++ user-service/src/main/java/com/example/user_service/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.example.user_service.repository;
+
+import com.example.user_service.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    // You can add custom queries here if needed
+}
